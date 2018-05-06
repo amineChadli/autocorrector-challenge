9542b768c2f1072f8fccd2774e1b118378912a2b
= <amine.chadli.ensias@gmail.com>
2018-05-06 17:34:15 +0200

    refactoring the code

--- bless_your_autocorrect.py
+++ bless_your_autocorrect.py
@@ -7,41 +7,32 @@ Created on Sun Aug 20 22:27:40 2017
 '''bless your autocorrect'''
 
 
-'''this function test if a string ch1 start with another string ch2'''
+# this function check if a string ch1 start with another string ch2'''
 def start_with(ch1,ch2):
-  #if the length of the second string is greater thant the length of the first one then we return false
-  if(len(ch2)>len(ch1)):
-    return False
-  #else and if the fist string ch1 starts with the second string then we return true
-  if(ch1[:len(ch2)]==ch2):
-    return True
-  #else we return false
-  return False
-  
+  return ch1.startswith(ch2)
 
-'''this function get the minimum triplet in a list
-using this order : (a,b,c) < (e,f,g) <--> b < f
-in our case b and f represent the difference between each string in the dictionnary and the word we look for '''
-def get_min(L):
-  c = (200,200,200)
-  for i in range(len(L)):
-    a = L[i]
-    if(a[1] < c[1] ):
-      c = a 
-  return c
-'''this function get the difference between two string by computer only the number of letters in the first string that are not in the second with the condition that ch1 start with ch2
-'''
+# this function get the minimum triplet in a list
+# using this order : (a,b,c) < (e,f,g) <--> b < f
+# in our case b and f represent the difference between each string in the dictionnary and the word we look for 
+def get_min_triplet(L):
+  return min(L, key = lambda x: x[1])
+
+# this function retuns the number of letters from the second string that are in the begining of the first string
+def get_num_similar(ch1,ch2):
+  q = 1
+  while(ch1[:q]==ch2[:q] and q<=min(len(ch1),len(ch2))):
+    q+=1
+  return (q-1)
+      
+# this function get the difference between two string by computing only the number of letters 
+# in the first string that are not in the second with the condition that ch1 start with ch2
 def dif_right(ch1,ch2):
-  q = 1
-  while(ch1[:q]==ch2[:q] and q<=min(len(ch1),len(ch2))):
-    q+=1
-  return len(ch1) - (q-1)
-'''the function do the same thing as the function dif_right but by getting the max difference between the two strings'''
+  return len(ch1) - get_num_similar(ch1,ch2)
+
+# this function do the same thing as the function dif_right but it returns the maximum difference between the two strings'''
 def dif(ch1,ch2):
-  q = 1
-  while(ch1[:q]==ch2[:q] and q<=min(len(ch1),len(ch2))):
-    q+=1
-  return max(len(ch1),len(ch2)) - (q-1)
+  return max(len(ch1),len(ch2)) - get_num_similar(ch1,ch2)
+
 '''and this is a function that returns a new_word from the previous word , a list of elemens and the number of buttons we clicked'''
 def compare(dict,word):
     sum=0
@@ -49,7 +40,6 @@ def compare(dict,word):
     #L=np.zeros(size)
     L=[]
     M=[]
-    
     i = 1
     ch = " "
     while(i<=size):
@@ -67,7 +57,7 @@ def compare(dict,word):
       sum+=min
       return "",[],sum
     else : 
-      c = get_min(L)
+      c = get_min_triplet(L)
       #print("c[2]" ,dict[c[2]])
       sum+=c[0]+1
       K = []
